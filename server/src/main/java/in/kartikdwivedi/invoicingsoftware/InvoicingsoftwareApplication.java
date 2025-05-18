e8e7e5657bb4caa8bfc7e68d42b21d8764037a7e
Kartik Dwivedi <codewithme.kartik@gmail.com>
2025-05-19 00:03:28 +0530

    server setup done

---
+++ server/src/main/java/in/kartikdwivedi/invoicingsoftware/InvoicingsoftwareApplication.java
@@ -0,0 +1,13 @@
+package in.kartikdwivedi.invoicingsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicingsoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicingsoftwareApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/in/kartikdwivedi/invoicingsoftware/InvoicingsoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kartikdwivedi.invoicingsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoicingsoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
